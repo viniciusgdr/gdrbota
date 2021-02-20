02a0293857ba00c3991d57a59af4a78ef242987a
viniciusgdr <79378773+viniciusgdr@users.noreply.github.com>
2021-02-20 20:05:23 -0300

    gdr

---
+++ Caussz-main/src/help.js
@@ -0,0 +1,111 @@
+﻿const help = (prefix) => {
+	return `     *VINICIUS BOT*
+ *informações*
+  🐊 Prefix: 「  ${prefix}  」
+ 
+  🐊│
+├─ 🐊 ${prefix}ping
+  *FAZER*
+  │
+ ├─ 🐊 ${prefix}menuadmin
+   ├─ 🐊 ${prefix}sticker
+  ├─ 🐊 ${prefix}stickergif
+  ├─ 🐊 ${prefix}toimg
+  ├─ 🐊 ${prefix}tomp3
+  ├─ 🐊 ${prefix}wolfmetal
+  ├─ 🐊 ${prefix}metalictglow
+  ├─ 🐊 ${prefix}8bit
+  ├─ 🐊 ${prefix}ttp
+  ├─ 🐊 ${prefix}herrypotter
+  ├─ 🐊 ${prefix}pubglogo
+  └─ 🐊 ${prefix}quotemaker
+◪🧐 *MEDIA*
+  │
+  ├─ 🐊 ${prefix}trendtwit
+  ├─ 🐊 ${prefix}randomkpop
+  └─ 🐊 ${prefix}ytsearch
+🧐 *escola*
+  │
+  ├─ 🐊 ${prefix}wiki
+  ├─ 🐊 ${prefix}wikien
+  ├─ 🐊 ${prefix}nulis
+  ├─ 🐊 ${prefix}quotes
+  ├─ 🐊 ${prefix}quotes2
+  └─ 🐊 ${prefix}artinama
+🧐 *KERANG AJAIB*
+  │
+  ├─ 🐊 ${prefix}apakah
+  ├─ 🐊 ${prefix}kapankah
+  ├─ 🐊 ${prefix}rate
+  └─ 🐊 ${prefix}bisakah
+🧐 *DOWNLOADER*
+  │
+  ├─ 🐊 ${prefix}images
+  ├─ 🐊 ${prefix}ytmp3
+  ├─ 🐊 ${prefix}ytmp4
+  ├─ 🐊 ${prefix}tiktok
+  └─ 🐊 ${prefix}joox
+🧐 *MEME*
+  │
+  ├─ 🐊 ${prefix}meme
+  └─ 🐊 ${prefix}memeindo
+🧐 *SOM*
+  │
+  ├─ 🐊 ${prefix}play
+  └─ 🐊 ${prefix}tts
+🧐 *MÚSICA*
+  │
+  ├─ 🐊 ${prefix}lirik
+  └─ 🐊 ${prefix}chord
+🧐 *ISLAM*
+  │
+  └─ 🐊 ${prefix}quran
+🧐 *STALK*
+  │
+  ├─ 🐊 ${prefix}tiktokstalk
+  └─ 🐊 ${prefix}igstalk
+🧐 *WIBU*
+  │
+  ├─ 🐊 ${prefix}neonime
+  ├─ 🐊 ${prefix}pokemon
+  ├─ 🐊 ${prefix}loli
+  ├─ 🐊 ${prefix}waifu
+  ├─ 🐊 ${prefix}randomanime
+  ├─ 🐊 ${prefix}husbu
+  ├─ 🐊 ${prefix}husbu2
+  ├─ 🐊 ${prefix}wait
+  └─ 🐊 ${prefix}nekonime
+🧐 *DIVERSÃO*
+  │
+  ├─ 🐊 ${prefix}alay
+  ├─ 🐊 ${prefix}gantengcek
+  ├─ 🐊 ${prefix}watak
+  ├─ 🐊 ${prefix}hobby
+  ├─ 🐊 ${prefix}game
+  ├─ 🐊 ${prefix}bucin
+  ├─ 🐊 ${prefix}trust
+  ├─ 🐊 ${prefix}dare
+  └─ 🐊 ${prefix}simi
+🧐 *INFORMAÇÃO*
+  │
+  ├─ 🐊 ${prefix}bahasa
+  ├─ 🐊 ${prefix}kodenegara
+  ├─ 🐊 ${prefix}kbbi
+  ├─ 🐊 ${prefix}fakta
+  ├─ 🐊 ${prefix}infocuaca
+  ├─ 🐊 ${prefix}infogempa
+  ├─ 🐊 ${prefix}jadwaltvnow
+  └─ 🐊 ${prefix}covid
+🧐 *COMANDOS PREMIUM*
+  │
+  ├─ 🐊 ${prefix}setprefix
+  ├─ 🐊 ${prefix}block
+  ├─ 🐊 ${prefix}bc
+  ├─ 🐊 ${prefix}bcgc
+  ├─ 🐊 ${prefix}clone
+  └─ 🐊 ${prefix}clearall
+🧐 
+`
+}
+
+exports.help = help
